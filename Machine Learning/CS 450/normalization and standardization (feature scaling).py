0f2bdf7550d97933de2b8c40e6520ee3dcf34a16
gustavo-hideo <36019434+gustavo-hideo@users.noreply.github.com>
2019-01-22 18:59:21 -0700

    Add files via upload

---
+++ Machine Learning/CS 450/normalization and standardization (feature scaling).py
@@ -0,0 +1,35 @@
+# NORMALIZATION AND STANDARDIZATION (FEATURE SCALLING)
+
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing   # Standrdization and Min-Max scaling
+
+
+# IMPORTING WINE DATASET
+df = pd.io.parsers.read_csv(
+    'https://raw.githubusercontent.com/rasbt/pattern_classification/master/data/wine_data.csv',
+     header=None,
+     usecols=[0,1,2]
+    )
+
+df.columns=['Class label', 'Alcohol', 'Malic acid']
+
+
+
+
+# STANDARDIZATION AND MIN-MAX SCALING
+std_scale = preprocessing.StandardScaler().fit(df[['Alcohol', 'Malic acid']])
+df_std = std_scale.transform(df[['Alcohol', 'Malic acid']])
+
+minmax_scale = preprocessing.MinMaxScaler().fit(df[['Alcohol', 'Malic acid']])
+df_minmax = minmax_scale.transform(df[['Alcohol', 'Malic acid']])
+
+print('Mean after standardization:\nAlcohol={:.2f}, Malic acid={:.2f}'
+      .format(df_std[:,0].mean(), df_std[:,1].mean()))
+print('\nStandard deviation after standardization:\nAlcohol={:.2f}, Malic acid={:.2f}'
+      .format(df_std[:,0].std(), df_std[:,1].std()))
+
+print('Min-value after min-max scaling:\nAlcohol={:.2f}, Malic acid={:.2f}'
+      .format(df_minmax[:,0].min(), df_minmax[:,1].min()))
+print('\nMax-value after min-max scaling:\nAlcohol={:.2f}, Malic acid={:.2f}'
+      .format(df_minmax[:,0].max(), df_minmax[:,1].max()))
